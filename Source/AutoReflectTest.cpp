d775f61cf134cdd449dc7f15dd3853bbc491cf53
Benny Wysong-Grass <bennywwg@gmail.com>
2023-03-05 12:01:23 -0600

    Fix for windows

--- Source/AutoReflectTest.cpp
+++ Source/AutoReflectTest.cpp
@@ -2,7 +2,6 @@
 #include <string>
 
 #include <AutoReflectTest.hpp>
-
 #include <AutoReflectTest.generated.inl>
 
 template<typename T>
@@ -15,13 +14,9 @@ void Log(T const& Val) {
 }
 
 int main(int argc, char** argv) {
-    ns::Vec<int> v;
-    v.x = 1;
-    v.y = 2;
-    v.z = 3;
+    ns::Vec<int> v = { {}, 1, 2, 3 };
+    Log(v);
 
     Person p3 { {}, 45, std::string("Robert") };
     Log(p3);
-
-    Log(v);
 }
