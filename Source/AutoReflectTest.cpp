6c1ca2cec2441f2e6fc46314fe347547c2dba4f5
Benny Wysong-Grass <bennywwg@gmail.com>
2023-03-17 14:08:56 -0500

    blah, don't push this

--- Source/AutoReflectTest.cpp
+++ Source/AutoReflectTest.cpp
@@ -12,20 +12,25 @@ void Log(T const& Val) {
 
     SerializeFields(Ser, Val);
 
-    std::cout << Ser.Data << std::endl;
+    std::cout << Ser.Data;
+
+    if (!Ser.Binary.empty()) {
+		std::cout << " (with binary data of " << Ser.Binary.size() << " bytes)";
+	}
+    std::cout << std::endl;
 }
 
 int main(int argc, char** argv) {
     Vec<int> v = { 1, 2, 3 };
     Log(v);
 
-    Person p3 { 45, std::string("Robert") };
+    Person p3 { 45, "Robert" };
     Log(p3);
 
     std::vector<Person> People;
     People.push_back(p3);
-    People.push_back(Person { 23, std::string("John") });
-    People.push_back(Person { 34, std::string("Jane") });
+    People.push_back(Person { 23, "John" });
+    People.push_back(Person { 34, "Jane" });
     Log(People);
 
     std::vector<Vec<int>> Vectors;
@@ -33,4 +38,17 @@ int main(int argc, char** argv) {
     Vectors.push_back(Vec<int> { 4, 5, 6 });
     Vectors.push_back(Vec<int> { 7, 8, 9 });
     Log(Vectors);
+
+    std::vector<std::vector<Vec<int>>> Vectors2;
+    Vectors2.push_back(Vectors);
+    Vectors2.push_back(Vectors);
+    Vectors2.push_back(Vectors);
+    Log(Vectors2);
+
+    std::vector<uint8_t> BinaryData;
+    BinaryData.push_back(0x01);
+    BinaryData.push_back(0x02);
+    BinaryData.push_back(0x03);
+    BinaryData.push_back(0x04);
+    Log(BinaryData);
 }
